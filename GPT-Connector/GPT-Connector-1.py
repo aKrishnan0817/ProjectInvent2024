85a5dfd5c2c1e34d14083ae932aed69a03d0cfcc
“Atul <atul.krishnan07@gmail.com>
2024-01-10 14:34:27 -0500

    Passed through the previos conversation logs into the bot so it can remeber the conversation

--- GPT-Connector/GPT-Connector-1.py
+++ GPT-Connector/GPT-Connector-1.py
@@ -15,8 +15,13 @@ os.environ['OPENAI_API_KEY'] =API_KEY
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 #Speech to text. Use OS speech-to-text app. For example,   Windows: press Windows Key + H
+iprompt = []
+assert1={"role": "system", "content": "You are a helpful assistant."}
+assert2={"role": "assistant", "content": "Geography is an important topic if you are going on a once in a lifetime trip."}
+iprompt.append(assert1)
+iprompt.append(assert2)
 
-def prepare_message():
+def prepare_message(iprompt):
   #enter the request with a microphone or type it if you wish
   # example: "Where is Tahiti located?"
   print("Enter a request and press ENTER:")
@@ -26,16 +31,16 @@ def prepare_message():
   #prompt="Where is Tahiti located?" #maintenance or if you do not want to use a microphone
   line = {"role": role, "content": uinput}
   #creating the message
-  assert1={"role": "system", "content": "You are a helpful assistant."}
-  assert2={"role": "assistant", "content": "Geography is an important topic if you are going on a once in a lifetime trip."}
+
   assert3=line
-  iprompt = []
-  iprompt.append(assert1)
-  iprompt.append(assert2)
+
   iprompt.append(assert3)
   return iprompt
+
+#run the cell to start/continue a dialog
+
 while(True):
-    iprompt = prepare_message()
+    iprompt=prepare_message(iprompt) #preparing the messages for ChatGPT
 
     response=openai.ChatCompletion.create(model="gpt-3.5-turbo",messages=iprompt) #ChatGPT dialog
 
