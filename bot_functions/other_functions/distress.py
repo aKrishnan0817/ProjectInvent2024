59cab71e2b867655d7613db4990d6293ef99bc86
zach-guo <zacharyguo@pierrepontschool.org>
2024-01-24 09:49:01 -0500

    distress finalized for MVP

--- bot_functions/other_functions/distress.py
+++ bot_functions/other_functions/distress.py
@@ -27,7 +27,14 @@ def send_email(subject,body):
     except Exception as e:
         print(f"Error: {e}")
 
-def main():
-    return
 
-send_email('test','hello world')
+#next step would be to engineer a different prompt for when in distress mode
+def main():
+    #we could have different messages for different severities but that isn't necessary at this moment
+    message = "Hi Hope, this is a notification that Jonah may be in a nervous state right now. Please check in on him as soon as you can."
+    subject = 'AI Companion: Notification for Jonah'
+    send_email(subject, message)
+
+    prompt = "You are now operating as a therapist who is consoling and helping a child in a moment of severe distress. We already contacted his mother who is on her way."
+    #return a new prompt for operating 
+    return prompt
