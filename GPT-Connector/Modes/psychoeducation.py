19682e1c77aede6d8c077585ed9e397eac226f75
hongyu447 <hongyuh2009@gmail.com>
2025-02-13 12:43:10 -0500

    Update psychoeducation.py

--- GPT-Connector/Modes/psychoeducation.py
+++ GPT-Connector/Modes/psychoeducation.py
@@ -14,13 +14,8 @@ from toolkit.tools import tools
 
 def psychoeducation(inputType, button):
     topic = chooseTopic(inputType,button)
-    if topic == "twentyQuestions":
-        return play20Questions(inputType,button)
-    if topic =="superheroTrivia":
-        #print("----playing super hero trivia----")
-        return playSuperheroTrivia(inputType,button)
-    if topic == "geoTrivia":
-        return playGeoTrivia(inputType,button)
+    return pschychoeducationTopic(inputType,button, topic)
+   
 
     if topic in ["story","stop","distress","coping"]:
         return topic
@@ -40,4 +35,15 @@ def chooseTopic(inputType, button):
         _,_,topic = prepare_message(iprompt, inputType , selectGameTools,button=button)
 
     return topic
+
+def psychoeducationTopic(inputType,button=None, topic):
+    geoTopicList = read_file_and_tokenize("Modes/gameModeTopics.txt")
+
+    iprompt = []
+    assert1={"role": "system", "content": "You are an ai friend to a child"}
+    randomGeoTopic = random.choice(geoTopicList)
+
+    content = f"""You are now a psychoeducation teacher. Your job is to teach a child information about "{topic}". When the child asks to start the game starts and you respond with one such trivia question. If the child gets the answer right, you say correct! And then ask the next question, if they get it wrong, give the child a hint and wait for their next response if their next response is correct, say correct! and ask the next question, if their next response is still incorect, just give the child another hint until they answer correctly. If after answering the question incorrectly the child says 'i give up', move on to the next question. you may never ever ever repeat questions"""
+
+    assert2={"role": "assistant", "content": content}
     
