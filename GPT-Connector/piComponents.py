255643810e26f184a559d877606025958776c7e4
Atul Krishnan <atul.krishnan07@gmail.com>
2025-05-08 12:34:16 -0400

    a should be fix

--- GPT-Connector/piComponents.py
+++ GPT-Connector/piComponents.py
@@ -1,60 +1,45 @@
 import time
-
-try:
-    from gpiozero import Button
-    import RPi.GPIO as GPIO
-except:
-    print("Failed to import gpiozero or RPi.GPIO. This likely means you're running on a non-Raspberry Pi environment.")
-    import traceback
-    traceback.print_exc()
-
+import RPi.GPIO as GPIO
 
 class piComponents:
-    # button is 2 and led is 4
     def __init__(self, buttonPin, ledPin):
         self.buttonPin = buttonPin
         self.ledPin = ledPin
-        print(f"[piComponents] Initialized with buttonPin={self.buttonPin}, ledPin={self.ledPin}")
+        print(f"[piComponents] Initializing with buttonPin={self.buttonPin}, ledPin={self.ledPin}")
         try:
-            print("[piComponents] GPIO mode set to BCM")
             GPIO.setmode(GPIO.BCM)
-            print("[piComponents] GPIO.cleanup() called to reset any previous configuration")
-            GPIO.cleanup()
+            GPIO.setup(self.buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
             GPIO.setup(self.ledPin, GPIO.OUT)
-            print(f"[piComponents] LED pin {self.ledPin} set as OUTPUT")
-            print(f"[piComponents] Attempting to initialize Button on GPIO pin {self.buttonPin}")
-            self.button = Button(self.buttonPin)
-            print("[piComponents] Button initialized successfully. Button will be used.")
+            print("[piComponents] GPIO setup successful.")
             self.buttonUse = True
-        except:
-            print("[piComponents][ERROR] Exception during GPIO/Button initialization. This may mean you're not running on a Raspberry Pi or the GPIO pins are already in use.")
+        except Exception as e:
+            print("[piComponents][ERROR] Failed to initialize GPIO.")
             import traceback
             traceback.print_exc()
-            print("[piComponents][ERROR] Tip: r conflicting uses of the GPIO pin. You can also call GPIO.cleanup() before setup.")
-
             self.buttonUse = False
 
     def checkButtonPress(self):
         if self.buttonUse:
             while True:
-                if self.button.is_pressed:
+                if GPIO.input(self.buttonPin) == GPIO.LOW:
                     self.setLed(1)
+                    print("[piComponents] Button pressed!")
                     time.sleep(1)
                     self.setLed(0)
                     return True
-
                 else:
                     self.setLed(0)
         return None
 
-    # 0 for off - 1 for on
     def setLed(self, onOff):
         if self.buttonUse:
             GPIO.setup(self.ledPin, GPIO.OUT)
             if onOff:
                 GPIO.output(self.ledPin, GPIO.HIGH)
-                print("turning led on")
-            GPIO.output(self.ledPin, GPIO.LOW)
+                print("[piComponents] LED ON")
+            else:
+                GPIO.output(self.ledPin, GPIO.LOW)
+                print("[piComponents] LED OFF")
 
     def getButtonUse(self):
         return self.buttonUse
