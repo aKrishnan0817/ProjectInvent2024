dc9c0d8debc43ee887fea74b5ba04fe93cc5a590
Aaron Wang <warhawk947@gmail.com>
2025-05-22 10:19:31 -0400

    temporary debugging 5

--- GPT-Connector/speech_to_text.py
+++ GPT-Connector/speech_to_text.py
@@ -71,7 +71,7 @@ def getSpeech(button):
             # Button is now pressed, LED is on
             try:
                 print("4. Listening...")
-                recognizer.adjust_for_ambient_noise(source)
+                #recognizer.adjust_for_ambient_noise(source)
                 # Set a timeout that's longer than expected button press
                 audio = recognizer.listen(source, timeout=30)
             except:
